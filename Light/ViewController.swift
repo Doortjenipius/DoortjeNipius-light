6b1eb8a66cd3771b5ff9b2efc2b808c7fc802fa9
Sophie Koffeman <sophiekoffeman@wcw-stud-145-109-40-185.wireless.uva.nl>
2018-11-02 13:57:39 +0100

    Ik heb de app gemaakt en een README bestand toegevoegd.

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -2,19 +2,27 @@
 //  ViewController.swift
 //  Light
 //
-//  Created by Sophie Koffeman on 02-11-18.
-//  Copyright © 2018 Sophie Koffeman. All rights reserved.
+//  Created by Doortje Nipius on 02-11-18.
+//  Copyright © 2018 Doortje Nipius. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    @IBOutlet weak var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+        
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black}
 }
-
---
+++ README.md
@@ -0,0 +1,3 @@
+### Light app
+
+In deze app kan je door middel van klikken het licht aan het uit zetten. De button reageert elke keer als je er op klikt door te veranderen van een witte achtergrond naar een zwarte achtergrond, of andersom.
