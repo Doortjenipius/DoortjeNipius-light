5649c6f8c05cf252d2e58eeb79d109a66241b64a
Sophie Koffeman <sophiekoffeman@wcw-stud-145-109-40-185.wireless.uva.nl>
2018-11-02 10:43:58 +0100

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Sophie Koffeman on 02-11-18.
+//  Copyright © 2018 Sophie Koffeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
